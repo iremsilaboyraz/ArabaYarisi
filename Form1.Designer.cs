82150f788000f4f2c44a53b4509bcd4fa6085429
İrem Sıla Boyraz <147523018+iremsilaboyraz@users.noreply.github.com>
2024-11-28 03:47:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.KullaniciArabasi = new System.Windows.Forms.PictureBox();
+            this.Araba1 = new System.Windows.Forms.PictureBox();
+            this.Araba2 = new System.Windows.Forms.PictureBox();
+            this.Araba3 = new System.Windows.Forms.PictureBox();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.oyunTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.KullaniciArabasi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // KullaniciArabasi
+            // 
+            this.KullaniciArabasi.Image = ((System.Drawing.Image)(resources.GetObject("KullaniciArabasi.Image")));
+            this.KullaniciArabasi.Location = new System.Drawing.Point(166, 339);
+            this.KullaniciArabasi.Name = "KullaniciArabasi";
+            this.KullaniciArabasi.Size = new System.Drawing.Size(50, 100);
+            this.KullaniciArabasi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.KullaniciArabasi.TabIndex = 0;
+            this.KullaniciArabasi.TabStop = false;
+            // 
+            // Araba1
+            // 
+            this.Araba1.Image = ((System.Drawing.Image)(resources.GetObject("Araba1.Image")));
+            this.Araba1.Location = new System.Drawing.Point(312, 260);
+            this.Araba1.Name = "Araba1";
+            this.Araba1.Size = new System.Drawing.Size(60, 100);
+            this.Araba1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Araba1.TabIndex = 1;
+            this.Araba1.TabStop = false;
+            // 
+            // Araba2
+            // 
+            this.Araba2.Image = ((System.Drawing.Image)(resources.GetObject("Araba2.Image")));
+            this.Araba2.Location = new System.Drawing.Point(228, 27);
+            this.Araba2.Name = "Araba2";
+            this.Araba2.Size = new System.Drawing.Size(45, 100);
+            this.Araba2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Araba2.TabIndex = 2;
+            this.Araba2.TabStop = false;
+            // 
+            // Araba3
+            // 
+            this.Araba3.Image = ((System.Drawing.Image)(resources.GetObject("Araba3.Image")));
+            this.Araba3.Location = new System.Drawing.Point(12, 122);
+            this.Araba3.Name = "Araba3";
+            this.Araba3.Size = new System.Drawing.Size(50, 100);
+            this.Araba3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Araba3.TabIndex = 3;
+            this.Araba3.TabStop = false;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(12, 9);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(75, 24);
+            this.lblSkor.TabIndex = 4;
+            this.lblSkor.Text = "Skor: 0";
+            // 
+            // oyunTimer
+            // 
+            this.oyunTimer.Interval = 20;
+            this.oyunTimer.Tick += new System.EventHandler(this.oyunTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.Araba3);
+            this.Controls.Add(this.Araba2);
+            this.Controls.Add(this.Araba1);
+            this.Controls.Add(this.KullaniciArabasi);
+            this.Name = "Form1";
+            this.Text = "Araba Oyunu";
+            ((System.ComponentModel.ISupportInitialize)(this.KullaniciArabasi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Araba3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox KullaniciArabasi;
+        private System.Windows.Forms.PictureBox Araba1;
+        private System.Windows.Forms.PictureBox Araba2;
+        private System.Windows.Forms.PictureBox Araba3;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Timer oyunTimer;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        private bool isMovingLeft = false;
+        private bool isMovingRight = false;
+        private Random rnd = new Random(); // Random nesnesini burada tanımladık
+        private int score = 0; // Skor değişkeni
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown); // KeyDown olayını ekleyin
+            this.KeyUp += new KeyEventHandler(Form1_KeyUp); // KeyUp olayını ekleyin
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) // Space tuşuna basıldığında
+            {
+                if (!oyunTimer.Enabled) // Oyun başlamadıysa
+                {
+                    oyunTimer.Start(); // Oyun başlat
+                }
+            }
+            if (e.KeyCode == Keys.Left) // Sol ok tuşuna basıldığında
+            {
+                isMovingLeft = true;
+            }
+            if (e.KeyCode == Keys.Right) // Sağ ok tuşuna basıldığında
+            {
+                isMovingRight = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) // Sol ok tuşu bırakıldığında
+            {
+                isMovingLeft = false;
+            }
+            if (e.KeyCode == Keys.Right) // Sağ ok tuşu bırakıldığında
+            {
+                isMovingRight = false;
+            }
+        }
+
+        private void oyunTimer_Tick(object sender, EventArgs e)
+        {
+            // Arabaların hareket etmesi
+            Araba1.Top += 5;
+            Araba2.Top += 5;
+            Araba3.Top += 5;
+
+            // Arabaların ekran dışına çıkarsa üst kısma geri dönmesi
+            if (Araba1.Top > this.ClientSize.Height)
+            {
+                Araba1.Top = -Araba1.Height;
+                Araba1.Left = rnd.Next(0, this.ClientSize.Width - Araba1.Width); // Random yeni konum
+                score++; // Skoru arttır
+            }
+
+            if (Araba2.Top > this.ClientSize.Height)
+            {
+                Araba2.Top = -Araba2.Height;
+                Araba2.Left = rnd.Next(0, this.ClientSize.Width - Araba2.Width); // Random yeni konum
+                score++; // Skoru arttır
+            }
+
+            if (Araba3.Top > this.ClientSize.Height)
+            {
+                Araba3.Top = -Araba3.Height;
+                Araba3.Left = rnd.Next(0, this.ClientSize.Width - Araba3.Width); // Random yeni konum
+                score++; // Skoru arttır
+            }
+
+            // Kullanıcı arabası hareketi (sağ ve sol ok tuşları)
+            if (isMovingLeft && KullaniciArabasi.Left > 0)
+            {
+                KullaniciArabasi.Left -= 10;
+            }
+            if (isMovingRight && KullaniciArabasi.Left < this.ClientSize.Width - KullaniciArabasi.Width)
+            {
+                KullaniciArabasi.Left += 10;
+            }
+
+            // Çarpışma kontrolü
+            if (KullaniciArabasi.Bounds.IntersectsWith(Araba1.Bounds) ||
+                KullaniciArabasi.Bounds.IntersectsWith(Araba2.Bounds) ||
+                KullaniciArabasi.Bounds.IntersectsWith(Araba3.Bounds))
+            {
+                oyunTimer.Stop(); // Oyun durduruluyor
+                MessageBox.Show("Çarpıştınız! Oyunu yeniden başlatın.", "Oyun Bitti", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                RestartGame(); // Oyunu sıfırlıyoruz
+            }
+
+            lblSkor.Text = $"Skor: {score}"; // Skoru güncelle
+        }
+
+        // Oyun sıfırlama metodu
+        private void RestartGame()
+        {
+            // Arabaları başlatma konumlarına sıfırlama
+            Araba1.Top = -Araba1.Height;
+            Araba1.Left = rnd.Next(0, this.ClientSize.Width - Araba1.Width);
+
+            Araba2.Top = -Araba2.Height;
+            Araba2.Left = rnd.Next(0, this.ClientSize.Width - Araba2.Width);
+
+            Araba3.Top = -Araba3.Height;
+            Araba3.Left = rnd.Next(0, this.ClientSize.Width - Araba3.Width);
+
+            KullaniciArabasi.Top = this.ClientSize.Height - KullaniciArabasi.Height - 10;
+            KullaniciArabasi.Left = this.ClientSize.Width / 2 - KullaniciArabasi.Width / 2;
+
+            score = 0; // Skoru sıfırlama
+            lblSkor.Text = "Skor: 0"; // Skoru sıfırlama
+            oyunTimer.Start(); // Oyun yeniden başlasın
+        }
+    }
+}
